855d4ad361a616eed7e9e9a338531b208b767c13
Jon Kuçi <jon.kuci@student.uni-pr.edu>
2025-03-17 15:36:55 +0100

    Added SpringBoot Project Structure. Must be connected to DB to function

---
+++ src/main/java/com/hrms/Human_Resource_Management_System_Back/HumanResourceManagementSystemBackApplication.java
@@ -0,0 +1,13 @@
+package com.hrms.Human_Resource_Management_System_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceManagementSystemBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceManagementSystemBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrms/Human_Resource_Management_System_Back/HumanResourceManagementSystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrms.Human_Resource_Management_System_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceManagementSystemBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
