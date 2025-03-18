d70bf8f51933b64d98d0db1d59081e98829d8de1
Jon Kuçi <jon.kuci@student.uni-pr.edu>
2025-03-18 15:44:16 +0100

    AuthenticationService, still fixing errors

--- src/main/java/com/hrms/Human_Resource_Management_System_Back/config/SecurityConfiguration.java
+++ src/main/java/com/hrms/Human_Resource_Management_System_Back/config/SecurityConfiguration.java
@@ -32,7 +32,7 @@ public class SecurityConfiguration {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
