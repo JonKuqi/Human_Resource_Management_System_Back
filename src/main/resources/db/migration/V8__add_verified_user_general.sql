26befe7ebf4b6a12697189f6f850fc4af85e106f
Jon Kuçi <jon.kuci@student.uni-pr.edu>
2025-05-10 00:13:07 +0200

    Fixed flyway. Is this the end?

--- src/main/resources/db/migration/V8__add_verified_user_general.sql
+++ src/main/resources/db/migration/V8__add_verified_user_general.sql
@@ -1,4 +1,4 @@
 
-ALTER TABLE user_general ADD COLUMN verified BOOLEAN DEFAULT FALSE NOT NULL;
-  ALTER TABLE public.verification_code
+ALTER TABLE public.user_general ADD COLUMN verified BOOLEAN DEFAULT FALSE NOT NULL;
+ALTER TABLE public.verification_code
       ALTER COLUMN code_hash TYPE VARCHAR(300);
