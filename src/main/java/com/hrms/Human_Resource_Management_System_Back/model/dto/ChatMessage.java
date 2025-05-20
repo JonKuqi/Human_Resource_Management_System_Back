d4bd646d3c0de7244b038807b8be47640b886b58
Erza <erzagashi04@gmail.com>
2025-05-20 22:42:48 +0200

    Save local changes before pulling main

---
+++ src/main/java/com/hrms/Human_Resource_Management_System_Back/model/dto/ChatMessage.java
@@ -0,0 +1,21 @@
+package com.hrms.Human_Resource_Management_System_Back.model.dto;
+
+
+import com.hrms.Human_Resource_Management_System_Back.model.types.MessageType;
+import lombok.*;
+
+import java.awt.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+}
